504ead59ff8c76e9549bf835696c87a99a7014fb
Adam L <skyros@gmail.com>
2019-01-19 11:08:19 +0800

    feat: reuse existing config

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM bitnami/minideb:stretch
-RUN apt-get update && apt-get install libuv1 -y
+RUN apt-get update && apt-get install libuv1 -y && rm -rf /var/cache/apt/
 COPY snell-server /usr/bin/snell-server
 COPY start.sh /start.sh
 
--- start.sh
+++ start.sh
@@ -2,15 +2,24 @@
 
 # docker run -e PSK=<your_psk_here> -p<your_host_port_here>:9102 --restart unless-stopped -d snell-server:latest
 
-if [ -z ${PSK} ]; then
-    echo Plese specify a valid PSK!
-    exit 1;
-fi
-
 BIN="/usr/bin/snell-server"
 CONF="/etc/snell-server.conf"
-echo "[snell-server]" >> ${CONF}
-echo "listen = 0.0.0.0:9102" >> ${CONF}
-echo "psk = ${PSK}" >> ${CONF}
-echo "obfs = http" >> ${CONF}
-${BIN} -c ${CONF}
+
+# reuse existing config when the container restarts
+if [ -f ${CONF} ]; then
+    echo "Found existing config..."
+    ${BIN} -c ${CONF}
+ else
+    if [ -z ${PSK} ]; then
+        PSK=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 16)
+        echo "Using generated PSK: ${PSK}"
+    else
+        echo "Using predefined PSK: ${PSK}"
+    fi
+    echo "Generating new config..."
+    echo "[snell-server]" >> ${CONF}
+    echo "listen = 0.0.0.0:9102" >> ${CONF}
+    echo "psk = ${PSK}" >> ${CONF}
+    echo "obfs = http" >> ${CONF}
+    ${BIN} -c ${CONF}
+fi
